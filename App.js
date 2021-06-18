4cc3241ef34663f07008fd649e58399b46391eeb
unais ali <51183141+unaisshazan@users.noreply.github.com>
2021-06-18 17:52:05 +0500

    add

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './screen/LoginScreen';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
 
 export default function App() {
     return ( 
         <NavigationContainer>  
-            
+            <Stack.Screen name="Login" component={LoginScreen} />
+             
         <View style={styles.container}>
         <StatusBar style="auto"></StatusBar>
         <Text>Lets build signal..</Text>
---
+++ screen/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function LoginScreen() {
+    return (
+        <View>
+            <Text>I am pippi</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
